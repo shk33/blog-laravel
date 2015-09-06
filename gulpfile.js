f593a3524e4143dd8d54f9d57fbe8c361af84826
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-06 14:10:46 -0500

    Add gulp copyfiles and merge files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var gulp   = require('gulp');
 var elixir = require('laravel-elixir');
 
 /*
@@ -10,8 +11,33 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+gulp.task("copyfiles", function() {
+  gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+    .pipe(gulp.dest("resources/assets/js/"));
 
+  gulp.src("vendor/bower_dl/bootstrap/less/**")
+    .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+    .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+    .pipe(gulp.dest("public/assets/fonts"));
+});
+
+/**
+* Default gulp is to run this elixir stuff
+*/
 elixir(function(mix) {
-    // mix.sass('app.scss');
-    mix.phpUnit();
+  // Combine scripts
+  mix.scripts([
+    'js/jquery.js',
+    'js/bootstrap.js'
+  ],
+  'public/assets/js/admin.js',
+  'resources/assets'
+  );
+
+  // Compile Less
+  mix.less('admin.less', 'public/assets/css/admin.css');
 });
