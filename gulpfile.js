c1f670114acd37dc41a0b567e89932580704fbf1
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-06 16:29:38 -0500

    Add fontawesome and datatbles less in admin.less and modify guklp to take care of these new assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp   = require('gulp');
+var rename = require('gulp-rename');
 var elixir = require('laravel-elixir');
 
 /*
@@ -23,6 +24,26 @@ gulp.task("copyfiles", function() {
 
   gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
     .pipe(gulp.dest("public/assets/fonts"));
+
+  gulp.src("vendor/bower_dl/font-awesome/less/**")
+    .pipe(gulp.dest("resources/assets/less/font-awesome"));
+
+  gulp.src("vendor/bower_dl/font-awesome/fonts/**")
+    .pipe(gulp.dest("public/assets/fonts"));
+
+  // Copy datatables
+  var dtDir = 'vendor/bower_dl/datatables-plugins/integration/';
+
+  gulp.src("vendor/bower_dl/datatables/media/js/jquery.dataTables.js")
+    .pipe(gulp.dest('resources/assets/js/'));
+
+  gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.css')
+    .pipe(rename('dataTables.bootstrap.less'))
+    .pipe(gulp.dest('resources/assets/less/others/'));
+
+  gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.js')
+    .pipe(gulp.dest('resources/assets/js/'));
+
 });
 
 /**
@@ -32,7 +53,9 @@ elixir(function(mix) {
   // Combine scripts
   mix.scripts([
     'js/jquery.js',
-    'js/bootstrap.js'
+    'js/bootstrap.js',
+    'js/jquery.dataTables.js',
+    'js/dataTables.bootstrap.js'
   ],
   'public/assets/js/admin.js',
   'resources/assets'
