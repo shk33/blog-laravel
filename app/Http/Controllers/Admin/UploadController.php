86a9a2174777818fe3adf62c6007b738b7dbfaaa
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-08 01:12:32 -0500

    Add UploadsController

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -1,13 +1,27 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\UploadsManager;
+use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    //
+  protected $manager;
+
+  public function __construct(UploadsManager $manager)
+  {
+    $this->manager = $manager;
+  }
+
+  /**
+  * Show page of files / subfolders
+  */
+  public function index(Request $request)
+  {
+    $folder = $request->get('folder');
+    $data = $this->manager->folderInfo($folder);
+    
+    return view('admin.upload.index', $data);
+  }
 }
