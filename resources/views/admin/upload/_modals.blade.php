3eeb64e74cdc7acc38be1eae1da49d77ae4b3582
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-11 01:17:08 -0500

    Add upload controller funcionality

--- resources/views/admin/upload/_modals.blade.php
+++ resources/views/admin/upload/_modals.blade.php
@@ -76,6 +76,45 @@
   </div>
 </div>
 
+{{-- Delete Folder Modal --}}
+<div class="modal fade" id="modal-folder-delete">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">
+          &times;
+        </button>
+        <h4 class="modal-title"> Please Confirm </h4>
+      </div>
+
+      <div class="modal-body">
+        <p class="lead">
+          <i class="fa fa-question-circle fa-lg"></i> &nbsp;
+          Are you sure you want to delete the
+          <kbd><span id="delete-folder-name1"> folder </span></kbd>
+          folder?
+        </p>
+      </div>
+
+      <div class="modal-footer">
+        <form method="POST" action="/admin/upload/folder">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_method" value="DELETE">
+          <input type="hidden" name="folder" value="{{ $folder }}">
+          <input type="hidden" name="del_folder" id="delete-folder-name2">
+          <button type="button" class="btn btn-default" data-dismiss="modal">
+            Cancel
+          </button>
+          <button type="submit" class="btn btn-danger">
+            Delete Folder
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
 {{-- Upload File Modal --}}
 <div class="modal fade" id="modal-file-upload">
   <div class="modal-dialog">
