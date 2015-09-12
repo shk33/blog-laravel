90bb363db5ac577a1253399b758e15581886c237
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-12 01:22:09 -0500

    add dates getters to Post model

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
   protected $dates = ['published_at'];
 
+  protected $fillable = [
+    'title', 'subtitle', 'content_raw', 'page_image', 
+    'meta_description', 'layout', 'is_draft', 
+    'published_at',
+  ];
   /**
   * The many-to-many relationship between posts and tags.
   *
@@ -80,5 +85,29 @@ class Post extends Model
     }
     $this->tags()->detach();
   }
-}
 
+  /**
+  * Return the date portion of published_at
+  */
+  public function getPublishDateAttribute($value)
+  {
+    return $this->published_at->format('M-j-Y');
+  }
+
+  /**
+  * Return the time portion of published_at
+  */
+  public function getPublishTimeAttribute($value)
+  {
+    return $this->published_at->format('g:i A');
+  }
+
+  /**
+  * Alias for content_raw
+  */
+  public function getContentAttribute($value)
+  {
+    return $this->content_raw;
+  }
+
+}
