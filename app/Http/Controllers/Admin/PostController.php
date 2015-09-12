7d799db76b24b4a46eeadb4d46bf61309e7cac8b
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-12 01:36:09 -0500

    Add all actions to Post controller except show action

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
+use App\Jobs\PostFormFields;
 use App\Http\Requests;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostUpdateRequest;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -16,7 +18,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('admin.post.index');
+        return view('admin.post.index')
+            ->withPosts(Post::all());
     }
 
     /**
@@ -26,29 +29,24 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $data = $this->dispatch(new PostFormFields());
+        return view('admin.post.create', $data);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param  PostCreateRequest  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        //
-    }
+        $post = Post::create($request->postFillData());
+        $post->syncTags($request->get('tags', []));
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('New Post Successfully Created.');
     }
 
     /**
@@ -59,19 +57,33 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data = $this->dispatch(new PostFormFields($id));
+        return view('admin.post.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  PostUpdateRequest  $request
      * @param  int  $id
      * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(PostUpdateRequest $request, $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->fill($request->postFillData());
+        $post->save();
+        $post->syncTags($request->get('tags', []));
+
+        if ($request->action === 'continue') {
+            return redirect()
+                ->back()
+                ->withSuccess('Post saved.');
+        }
+
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('Post saved.');
     }
 
     /**
@@ -82,6 +94,12 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->tags()->detach();
+        $post->delete();
+        
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('Post deleted.');
     }
 }
