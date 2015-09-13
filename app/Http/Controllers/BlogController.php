50d2af50df375f2dcd615ec2ff91e9e062760790
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-13 14:29:23 -0500

    Add disqus comments

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -29,6 +29,6 @@ class BlogController extends Controller
       $tag = Tag::whereTag($tag)->firstOrFail();
     }
     
-    return view($post->layout, compact('post', 'tag'));
+    return view($post->layout, compact('post', 'tag','slug'));
   }
 }
